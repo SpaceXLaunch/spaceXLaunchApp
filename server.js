6a818bb828fd42fd9d226bb05a3c7b74c8c6f908
BhattiBytes <bhattibytes@gmail.com>
2021-06-25 00:45:38 -0400

    Comment Server-side

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const cors = require('cors');
-const schema = require('./schema');
+// const express = require('express');
+// const { graphqlHTTP } = require('express-graphql');
+// const cors = require('cors');
+// const schema = require('./schema');
 
-const app = express();
+// const app = express();
 
-// Allow CORS
-app.use(cors());
+// // Allow CORS
+// app.use(cors());
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true  
-}));
+// app.use('/graphql', graphqlHTTP({
+//   schema,
+//   graphiql: true  
+// }));
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => (console.log(`Server is listening on port ${PORT}`)));
+// app.listen(PORT, () => (console.log(`Server is listening on port ${PORT}`)));
